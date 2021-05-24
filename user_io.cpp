a8b1dedabe3e9c11ee559dc45bc01abd2bca53b9
Karl Lurman <karl.lurman@wilsongroupau.com>
2021-05-24 14:11:09 +1000

    Msu1 - experimenting with ext and file sending for sending across audio data

--- user_io.cpp
+++ user_io.cpp
@@ -2287,6 +2287,13 @@ int user_io_file_tx(const char* name, unsigned char index, char opensave, char m
 	}
 
 	ProgressMessage(0, 0, 0, 0);
+
+    if (is_snes())
+    {
+        // Setup MSU
+        snes_msu_init(name);
+    }
+
 	return 1;
 }
 
@@ -2623,6 +2630,7 @@ void user_io_poll()
 	else if ((core_type == CORE_TYPE_8BIT) && !is_menu() && !is_minimig())
 	{
 		if (is_st()) tos_poll();
+		if (is_snes()) snes_poll();
 
 		while (1)
 		{
